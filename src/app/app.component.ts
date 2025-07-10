dd9bf48dedf649610c322a16e956eff4fa149e2a
maxhanna <maxahanna@hotmail.com>
2025-07-10 07:48:30 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-rsvp',
+  templateUrl: './rsvp.component.html',
+  styleUrls: ['./rsvp.component.css']
 })
-export class AppComponent {
-  title = 'angular-test';
+export class RsvpComponent {
+  rsvpForm: FormGroup;
+  submitted = false;
+  isLoading = false;
+  responseMessage = '';
+
+  // Placeholder for database service (to be implemented later)
+  // private rsvpService: RsvpService
+
+  constructor(private fb: FormBuilder) {
+    this.rsvpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      attending: ['', Validators.required],
+      guestCount: [1, [Validators.min(1), Validators.max(4)]],
+      dietaryRestrictions: [''],
+      songRequest: [''],
+      message: ['']
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.rsvpForm.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+
+    // Placeholder for database submission
+    // this.rsvpService.submitRsvp(this.rsvpForm.value).subscribe(
+    //   response => {
+    //     this.isLoading = false;
+    //     this.responseMessage = 'Thank you for your RSVP! We look forward to celebrating with you.';
+    //     this.rsvpForm.reset();
+    //     this.submitted = false;
+    //   },
+    //   error => {
+    //     this.isLoading = false;
+    //     this.responseMessage = 'There was an error submitting your RSVP. Please try again later.';
+    //   }
+    // );
+
+    // Simulating API call timeout
+    setTimeout(() => {
+      this.isLoading = false;
+      this.responseMessage = 'Thank you for your RSVP! We look forward to celebrating with you.';
+      this.rsvpForm.reset();
+      this.submitted = false;
+    }, 1500);
+  }
+
+  get f() { return this.rsvpForm.controls; }
 }
